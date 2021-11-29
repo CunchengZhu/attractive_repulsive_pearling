9d62472b5d9b4690e36d285025b7c3c316312b7a
cuncheng zhu <zhucuncheng@gmail.com>
2021-11-28 17:43:42 -0800

    chore: causal update of run drivers

--- run/driver.py
+++ run/driver.py
@@ -6,7 +6,7 @@ import parameters
 #                 Initialize pathes                #
 ####################################################
 """ Linux """
-outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/temp2"
+outputDir = "/home/cuzhu/attractive_repulsive_pearling/results/temp"
 
 """ Windows """
 # outputDir = (
@@ -14,13 +14,15 @@ outputDir = "/home/cuzhu/2020-Mem3DG-Applications/results/temp2"
 # )
 
 # trajFile = "/home/cuzhu/2020-Mem3DG-Applications/results/bud/testrefactor3/traj.nc"
+inputMesh = "/home/cuzhu/attractive_repulsive_pearling/run/input-file/hemisphere.obj"
+# trajFile = "/home/cuzhu/attractive_repulsive_pearling/results/temp/traj.nc"
 
 ####################################################
 #            Initialize input geometry             #
 ####################################################
 """ Built-in construction """
 # patFace, patVertex = dg.getHexagon(1, 4)
-icoFace, icoVertex = dg.getIcosphere(1, 3)
+# icoFace, icoVertex = dg.getIcosphere(1, 3)
 # tetFace, tetVertex = dg.getTetrahedron()
 # diaFace, diaVertex = dg.getDiamond(3.14/3)
 # cyFace, cyVertex = dg.getCylinder(1, 16, 60, 7.5, 0)
@@ -48,7 +50,7 @@ mP.meshMutator.flipNonDelaunay = True
 mP.meshMutator.splitFat = True
 mP.meshMutator.splitSkinnyDelaunay = True
 mP.meshMutator.splitCurved = True
-mP.meshMutator.curvTol = 0.005
+mP.meshMutator.curvTol = 0.002
 mP.meshMutator.collapseSkinny = True
 
 # mP.meshRegularizer.Kst = 0.1 # 2e-6
@@ -65,22 +67,23 @@ isContinue = False
 
 """ System construction """
 # g = dg.System(inputMesh, nSub)
-# g = dg.System(inputMesh, p, nSub, isContinue)
+g = dg.System(inputMesh, p, mP, nSub)
 # g = dg.System(soupFace, soupVertex, p, nSub)
-g = dg.System(icoFace, icoVertex, p, mP, nSub)
+# g = dg.System(icoFace, icoVertex, p, mP, nSub)
 # g = dg.System(patFace, patVertex, p, nSub)
 # g = dg.System(diaFace, diaVertex, diaVertex, nSub, p)
-# g = dg.System(trajFile, -1, nSub, False, p)
+# g = dg.System(trajFile, -1, p, mP, nSub)
 # g = dg.System(cyFace, cyVertex, p, nSub)
 
+# g.saveRichData(outputDir+"/hemisphere.obj", True)
 ###################################################
 #          Time integration / Optimization
 ####################################################
 """ Integrator setups (essential) """
-h = 1
+h = 0.1
 T = 10000000
 eps = 1e-6
-tSave = 300
+tSave = 30
 verbosity = 5
 
 """ Integrator construction """
